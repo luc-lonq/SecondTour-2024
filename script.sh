d07a24c446b08c42d69c0b9b4658049b19e42cff
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-06-06 11:06:26 +0200

    build(back): setup install test for (i=0; i<999999; i++) console.log(i) //15

--- script.sh
+++ script.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-cd stack/api/secondtour_api_v2
-apt-get update
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - sudo apt install -y nodejsnvm install
-npm i
-cd ../..
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+cd second-tour/stack
 docker compose up --build
