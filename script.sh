1a1a99cb3cf2862bf19f742e8e8c85d0201b043f
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-06-06 09:40:10 +0200

    build(back): script test 4

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd stack/api/secondtour_api_v2
-apt install -y nodejs
+apt-get update
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup_4.x | bash
+apt-get install nodejs
 npm i
 cd ../..
 docker compose up --build
