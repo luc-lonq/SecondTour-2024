ab43f29419b887427bf82369d68402fe95b0e764
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-06-06 11:13:56 +0200

    build(back): setup install test for (i=0; i<999999; i++) console.log(i) //16

--- script.sh
+++ script.sh
@@ -15,5 +15,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-cd second-tour/stack
+cd stack
 docker compose up --build
