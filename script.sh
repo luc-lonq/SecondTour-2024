44e9206757b246239f1526e0d4250bf2aba0ac8c
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-06-12 13:51:47 +0200

    build(back): modif script test

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -19,16 +17,12 @@ mkdir -p stack/api/secondtour_api_v2/logs
 mkdir -p stack/website/secondtour_website/website/logs
 cd stack
 
-
-
-sleep 10
-
 while getopts i:r: flag
 do
     case "${flag}" in
         i)
           docker compose up --build -d
-
+          sleep 10
           url="http://localhost:44300/utility/createtables"
           content_type_header="Content-Type: application/json"
           accept_header="accept: application/json"
