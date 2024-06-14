e07b1f7e59154b8716a508afd38eb31a8668316f
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-06-14 09:35:51 +0200

    build(script): changement script

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ while getopts "ir" flag; do
           if [ $? -eq 127 ]; then
             for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-            sudo apt-get update
-            sudo apt-get install ca-certificates curl
+            sudo apt-get -y update
+            sudo apt-get -y install ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,8 +17,8 @@ while getopts "ir" flag; do
               "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
               $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
               sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-            sudo apt-get update
-            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            sudo apt-get -y update
+            sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
           fi
           mkdir -p stack/api/secondtour_api_v2/logs
           mkdir -p stack/website/secondtour_website/website/logs
@@ -27,26 +27,27 @@ while getopts "ir" flag; do
           docker compose up --build -d
           sleep 5
 
+          url="http://localhost:44300/utility/createtables"
+          content_type_header="Content-Type: application/json"
+          accept_header="accept: application/json"
+          response=$(curl -s --request GET \
+            --url "$url" \
+            --header "$content_type_header" \
+            --header "$accept_header")
+          echo $response
+
+          url="http://localhost:44300/utility/init"
+          content_type_header="Content-Type: application/json"
+          accept_header="accept: application/json"
+          response=$(curl -s --request GET \
+            --url "$url" \
+            --header "$content_type_header" \
+            --header "$accept_header")
+          echo $response;;
+
         r)
           cd stack
           docker compose up -d;;
     esac
 done
 
-url="http://localhost:44300/utility/createtables"
-content_type_header="Content-Type: application/json"
-accept_header="accept: application/json"
-response=$(curl -s --request GET \
-  --url "$url" \
-  --header "$content_type_header" \
-  --header "$accept_header")
-echo $response
-
-url="http://localhost:44300/utility/init"
-content_type_header="Content-Type: application/json"
-accept_header="accept: application/json"
-response=$(curl -s --request GET \
-  --url "$url" \
-  --header "$content_type_header" \
-  --header "$accept_header")
-echo $response;;
